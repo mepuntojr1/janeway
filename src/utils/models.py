24b5f9526489fd5d952c2f5d31e24a319899a111
Joseph Muller <j.muller@bbk.ac.uk>
2022-11-29 13:55:57 +0000

    #3256 Brush up admin views

--- src/utils/models.py
+++ src/utils/models.py
@@ -159,6 +159,9 @@ class ToAddress(models.Model):
     def __str__(self):
         return self.email
 
+    class Meta:
+        verbose_name_plural = 'to addresses'
+
 
 class Version(models.Model):
     number = models.CharField(max_length=10)
